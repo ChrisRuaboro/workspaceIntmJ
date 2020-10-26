d98df0f2af3b9e2b28a8034821cdd6a2505e3816
ChrisRuaboro <52970508+ChrisRuaboro@users.noreply.github.com>
2020-10-26 12:34:24 -0700

    Unit tests baby!

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,29 @@
 package com.math;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorTest
+{
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @Test
+    public void testAdd()
+    {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+    @Test
+    public void testDivide()
+    {
+        Calculator calc = new Calculator();
+        assertEquals(3,calc.divide(9,3), .001);
+
+    }
+    @Test
+    public void testIsEqual()
+    {
+        Calculator calc = new Calculator();
+        assertTrue(calc.isEven(698));
+    }
 }
