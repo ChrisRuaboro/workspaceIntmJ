f634be16ce15846be935a582eb80b5504a8b926f
ChrisRuaboro <52970508+ChrisRuaboro@users.noreply.github.com>
2020-10-27 15:34:33 -0700

    TestsTests Tests

--- Lab04.1/src/com/entertainment/DisplayType.java
+++ Lab04.1/src/com/entertainment/DisplayType.java
@@ -8,4 +8,5 @@
  */
 package com.entertainment;
 
-public enum DisplayType { LCD, LED, OLED, PLASMA, CRT }
+public enum DisplayType
+{LCD, LED, OLED, PLASMA, CRT}
--- Lab04.1/test/com/entertainment/catalog/CatalogTest.java
+++ Lab04.1/test/com/entertainment/catalog/CatalogTest.java
@@ -9,19 +9,68 @@
 package com.entertainment.catalog;
 
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
+
 import org.junit.Test;
 import com.entertainment.Television;
 
-public class CatalogTest {
-  
-  /**
-   * Contract: a no-matches result should be an empty collection (not null).
-   */
-  @Test
-  public void testFindByBrandNoMatches() {
-    Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
-    assertNotNull(tvs);
-    assertTrue(tvs.isEmpty());
-  }
+public class CatalogTest
+{
+
+    /**
+     * Contract: a no-matches result should be an empty collection (not null).
+     */
+
+
+
+
+    @Test
+    public void testFindByBrands_shouldReturnNonnullEmptyMap_whenNoBrandsPassed()
+    {
+        Map<String, Collection<Television>> tvMap = Catalog.findByBrands();
+        assertNotNull(tvMap);
+        assertTrue(tvMap.isEmpty());
+
+    }
+
+    @Test
+    public void testFindByBrands_shouldReturnNotEmptyMap_whenBrandsPassed()
+    {
+        String[] brands = {"Sony","RCA"};
+        Map<String, Collection<Television>> tvMap = Catalog.findByBrands(brands);
+        assertNotNull(tvMap);
+        assertFalse(tvMap.isEmpty());
+        for (String tvBrand : tvMap.keySet()) {
+            assertTrue(Arrays.asList(brands).contains(tvBrand));
+        }
+
+    }
+
+    @Test
+    public void testFindByBrand_shouldReturnNotEmptyCollection_whenMatchingBrand()
+    {
+        // make the business call
+        String brand = "Zenith";
+        Collection<Television> tvs = Catalog.findByBrand(brand);
+
+        // Verify the result - what SHOULD be true about the 'tvs' collection?
+        assertNotNull(tvs);
+        assertFalse(tvs.isEmpty());
+        assertEquals(9, tvs.size());
+        for (Television tv : tvs) {
+            assertEquals(brand, tv.getBrand());
+        }
+    }
+
+    @Test
+    public void testFindByBrand_shouldReturnAnEmptyCollectionIfNoMatches()
+    {
+        Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
+        assertNotNull(tvs);
+        assertTrue(tvs.isEmpty());
+    }
 }
